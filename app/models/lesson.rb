52eba62d2f5e1eccb5cec2fb9ee88f52098bc9d9
marcomorawec <marco.morawec@gmail.com>
2013-07-27 00:09:09 -0400

    making lesson db fields accessible

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,13 @@
 class Lesson < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible 	:name, :week, :main_headline, :weekly_tagline, :first_ressource_headline,
+  					:first_ressource_text, :second_ressource_headline, :second_ressource_text,
+  					:third_ressource_headline, :third_ressource_text, :fourth_ressource_headline,
+  					:fourth_ressource_text, :fifth_ressource_headline, :fifth_ressource_text,
+  					:curator_name, :curator_description,
+  					:first_ressource_headline_link, :second_ressource_headline_link, 
+  					:third_ressource_headline_link, :fourth_ressource_headline_link,
+  					:fifth_ressource_headline_link
+
+
 end
+
