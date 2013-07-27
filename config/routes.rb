3273c79807bdd84acdd279e8ac37559f55e94d0f
marcomorawec <marco.morawec@gmail.com>
2013-07-27 00:15:10 -0400

    adding devise, user migration and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Startuphackme::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+
+  resources :lessons
+  resources :subscriber
+
+
+
+  root :to => 'subscriber#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -54,5 +63,5 @@ Startuphackme::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 end
