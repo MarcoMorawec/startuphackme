3c2867cb030d291c5ecac4f3439e1668cd5578a0
marcomorawec <marco.morawec@gmail.com>
2013-07-26 23:47:49 -0400

    adding all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startuphackme::Application.config.secret_token = '73e2f133a384a2c041a8d831e164c04d9fb19c0c2bff6fba4c4740d059e9011d791f7de36d2ea786e8a4a2aa162f9ad974c444ea1315a445fa9c91ef30a48ee8'
