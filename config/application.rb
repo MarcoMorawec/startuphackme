285d1437209bbf428e81c747a2362240c060e3fa
Isaac Girard <git@isaacgirard.com>
2013-07-27 23:01:26 -0400

    fix fonts - epic win - more tweaking and tuning on the subscriber index

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,11 @@ module Startuphackme
     #making devise procompiling work on heroku
     config.assets.initialize_on_precompile = false
 
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
