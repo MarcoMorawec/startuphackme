73a4f7366f24e3bc9033bd40fefcdf5b1909a422
marcomorawec <marco.morawec@gmail.com>
2013-08-20 12:35:53 -0400

    adding the super-secret-site to set cookies in internal browsers to filter out internal google analytics traffic noise

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Startuphackme::Application.routes.draw do
 
   root :to => 'subscriber#index'
 
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
