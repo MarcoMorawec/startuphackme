6c1e5e8fe83cb524930dc4264895c40a2869a313
marcomorawec <marco.morawec@gmail.com>
2013-09-13 16:08:53 -0400

    creating the static pages controller properly

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Startuphackme::Application.routes.draw do
 
   resources :lessons
   resources :subscriber
-  resources :privacy
+  # resources :privacy
+
+  get 'privacy', :to => 'static#privacy'
 
 
 
